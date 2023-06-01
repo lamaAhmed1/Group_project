6de0692bdbe6232cf4077c21caafc1c7abb9915e
Lama <azlama72@gmail.com>
2023-06-01 14:53:18 +0300

    commit first lama

---
+++ src/cpit251_project/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpit251_project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Main {
+      List<Product> stud=new ArrayList();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    private void showMenue(){
+        System.out.println("****************************************************************************"
+                         + "****************************************************************************"
+                         + "                       Welcome to Artsy application                         "
+                         + "****************************************************************************");
+    }
+    
+}
