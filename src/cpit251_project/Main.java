9fe770185c1db3b3368dd5a6eb0757373cef9b26
Lama <azlama72@gmail.com>
2023-06-01 16:10:29 +0300

    commit 3 lama

--- src/cpit251_project/Main.java
+++ src/cpit251_project/Main.java
@@ -8,6 +8,7 @@ package cpit251_project;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -20,18 +21,35 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input=new Scanner(System.in);
         
+        showMenue();
+        
+        int user1=input.nextInt();
+      
+        while(user1!=1 || user1!=2){
+           System.out.println("wrong input, please try again");
+           String x=input.nextLine();
+           char i=x.charAt(0);
+           if(x=="2"||x=="1"){
+               break;
+              
+           }
+           
+       }
+        
+       
         
     }
-    private void showMenue(){
+    public static void showMenue(){
         System.out.println("****************************************************************************"
-                         + "****************************************************************************"
-                         + "                       Welcome to Artsy application                         "
-                         + "****************************************************************************"
-                         + "****************************************************************************"
-                         + "who are you"
-                + "press (1) if you are a customer"
-                + "press (2) if you are an artist");
+                         + "\n****************************************************************************"
+                         + "\n                       Welcome to Artsy application                         "
+                         + "\n****************************************************************************"
+                         + "\n****************************************************************************"
+                         + "\nwho are you"
+                + "\npress (1) if you are a customer"
+                + "\npress (2) if you are an artist");
     }
     
 }
