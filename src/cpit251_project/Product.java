470cfa122e2b42b635552fd3a07421c79ae81ea7
Lama <azlama72@gmail.com>
2023-06-01 16:17:07 +0300

    commit 3 lama

--- src/cpit251_project/Product.java
+++ src/cpit251_project/Product.java
@@ -10,5 +10,28 @@ package cpit251_project;
  * @author lenovo
  */
 public class Product {
+    double price;
+    String type;
+
+    public Product(double price, String type) {
+        this.price = price;
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
     
 }
